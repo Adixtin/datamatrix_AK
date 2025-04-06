0f1c7214f233c9b3666a013489fef8b49a3b4444
monke <u128_adakuz_waw@technischools.com>
2025-04-06 18:48:48 +0200

    string_to_binary

--- app/app.py
+++ app/app.py
@@ -1,1 +1,6 @@
-print("datamatrix stuff")
+a = list(input())
+def string_to_binary(char_list: list) -> list[tuple]:
+    list_of_tuples = []
+    for i in char_list:
+        list_of_tuples.append(tuple(format(ord(i), '08b')))
+    return list_of_tuples
