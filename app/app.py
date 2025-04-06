1887dbae8eb34bb1be1305639bcd332af388edcc
monke <u128_adakuz_waw@technischools.com>
2025-04-07 00:25:16 +0200

    Datamatrix_class

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
+class Datamatrix:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        self.string_to_binary(self.data)
+
+
+    def string_to_binary(char_list: list) -> list[tuple]:
+        list_of_tuples = []
+        for i in char_list:
+            list_of_tuples.append(tuple(format(ord(i), '08b')))
+        return list_of_tuples
+
+
 a = list(input())
-def string_to_binary(char_list: list) -> list[tuple]:
-    list_of_tuples = []
-    for i in char_list:
-        list_of_tuples.append(tuple(format(ord(i), '08b')))
-    return list_of_tuples
